1fb88ee22c720b8fb4eef4c5184198fdaea2d8e2
Jukka Villstedt <jukka.villstedt@iki.fi>
2011-02-14 22:12:25 +0200

    added runnable apigenerator jar

---
+++ org.sidos.sbt.apigenerator/src/main/scala/org/sidos/sbt/apigenerator/APIGeneratorApplication.scala
@@ -0,0 +1,11 @@
+package org.sidos.sbt.apigenerator
+
+
+object APIGeneratorApplication
+{
+  def main(args: Array[String]) {
+    println(args(0))
+    //new org.sidos.generation.Generator(args[0])
+  }
+
+}
